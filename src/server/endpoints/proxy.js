68cf68d88ec7dbf97a058bf2f76f469c60437c7e
Raathigeshan Kugarajan <raathigeshank@zone24x7.com>
2016-06-23 22:47:00 +0530

    Remove preserveHostHdr in proxy endpoint

--- src/server/endpoints/proxy.js
+++ src/server/endpoints/proxy.js
@@ -11,8 +11,7 @@ function proxyModule(app, spec) {
       intercept: function(rsp, data, req, responseOut, callback) {
         responseOut.set('Access-Control-Allow-Origin', '*');
         callback(null, data);
-      },
-      preserveHostHdr: true
+      }
     }));
   }
 
