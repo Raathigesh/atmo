f3d44087e02b78d9a745c9d3ef75fe09972e09c6
Raathigeshan Kugarajan <raathigeshank@zone24x7.com>
2016-08-17 22:00:13 +0530

    Handle pre-flight requests for CORS. Closes #32

--- src/server/endpoints/http.js
+++ src/server/endpoints/http.js
@@ -1,4 +1,15 @@
 function httpModule(app, spec) {
+
+  /**
+   * To support CORS pre-flight requests.
+   */
+  app.options("/*", function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.send(200);
+  });
+
   for (var i = 0; i < spec.endpoints.length; i++) {
     addRoute(app, spec.endpoints[i]);
   }
