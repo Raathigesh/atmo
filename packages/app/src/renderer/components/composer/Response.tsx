2fd56eccde1d38e314512e6411415f55cd78b952
Raathigeshan Kugarajan <raathigesh92@gmail.com>
2017-08-24 22:24:57 +1000

    Warning on switching response type

--- packages/app/src/renderer/components/composer/Response.tsx
+++ packages/app/src/renderer/components/composer/Response.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
-import { Menu, Icon } from "semantic-ui-react";
+import { Menu, Icon, Modal, Header, Button } from "semantic-ui-react";
 import { observer } from "mobx-react";
+
 interface IResponse {
   activeItem?: string;
   setActiveItem: (value: string) => void;
 }
 
 interface State {
-  activeItem?: string;
+  isConfirmOpen?: boolean;
+  clickedItem?: string;
 }
 
 @observer
@@ -15,49 +17,90 @@ export default class MenuExampleBasic extends React.Component<
   IResponse,
   State
 > {
-  handleItemClick = (e, { name }) => this.props.setActiveItem(name);
+  state = {
+    isConfirmOpen: false,
+    clickedItem: ""
+  };
+
+  handleItemClick = (e, { name }) => {
+    this.setState({
+      isConfirmOpen: true,
+      clickedItem: name
+    });
+  };
+
+  onConfirm = () => {
+    this.setState({
+      isConfirmOpen: false,
+      clickedItem: ""
+    });
+    this.props.setActiveItem(this.state.clickedItem);
+  };
+
+  onClose = () => {
+    this.setState({
+      isConfirmOpen: false,
+      clickedItem: ""
+    });
+  };
 
   render() {
     const { activeItem } = this.props;
 
     return (
-      <Menu>
-        <Menu.Item
-          name="json"
-          active={activeItem === "json"}
-          onClick={this.handleItemClick}
-        >
-          <Icon name="quote left" />
-          Json
-        </Menu.Item>
+      <div>
+        <Menu>
+          <Menu.Item
+            name="json"
+            active={activeItem === "json"}
+            onClick={this.handleItemClick}
+          >
+            <Icon name="quote left" />
+            Json
+          </Menu.Item>
 
-        <Menu.Item
-          name="xml"
-          active={activeItem === "xml"}
-          onClick={this.handleItemClick}
-        >
-          <Icon name="file code outline" />
-          Xml
-        </Menu.Item>
+          <Menu.Item
+            name="xml"
+            active={activeItem === "xml"}
+            onClick={this.handleItemClick}
+          >
+            <Icon name="file code outline" />
+            Xml
+          </Menu.Item>
 
-        <Menu.Item
-          name="text"
-          active={activeItem === "text"}
-          onClick={this.handleItemClick}
-        >
-          <Icon name="file text outline" />
-          Text
-        </Menu.Item>
+          <Menu.Item
+            name="text"
+            active={activeItem === "text"}
+            onClick={this.handleItemClick}
+          >
+            <Icon name="file text outline" />
+            Text
+          </Menu.Item>
 
-        <Menu.Item
-          name="javascript"
-          active={activeItem === "javascript"}
-          onClick={this.handleItemClick}
-        >
-          <Icon name="code" />
-          Custom
-        </Menu.Item>
-      </Menu>
+          <Menu.Item
+            name="javascript"
+            active={activeItem === "javascript"}
+            onClick={this.handleItemClick}
+          >
+            <Icon name="code" />
+            Custom
+          </Menu.Item>
+        </Menu>
+        <Modal open={this.state.isConfirmOpen} basic size="small">
+          <Header icon="bomb" content="You will loose the content" />
+          <Modal.Content>
+            <p>This will reset the current content in the editor. Proceed?</p>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button basic color="red" inverted onClick={this.onClose}>
+              <Icon name="remove" /> No
+            </Button>
+            <Button color="green" inverted onClick={this.onConfirm}>
+              <Icon name="checkmark" /> Yes
+            </Button>
+          </Modal.Actions>
+        </Modal>
+      </div>
     );
   }
 }
